05d00ff7c31a8b5385315322ca774fafb06e8be5
fatualux <35587292+fatualux@users.noreply.github.com>
2023-11-06 20:31:25 +0100

    Changed drawer to vertical scrollable list

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use gtk::prelude::*;
-use gtk::{Button, Grid, Entry, EntryExt, Window, WindowType};
+use gtk::{Button, Box, Entry, EntryExt, Window, WindowType, ScrolledWindow};
 use serde::Deserialize;
 use std::sync::Arc;
+use gtk::Adjustment;
 
 #[derive(Deserialize, Clone)]
 struct App {
@@ -20,18 +21,17 @@ fn main() {
 
     let window = Window::new(WindowType::Toplevel);
     window.set_title("App Drawer");
-    window.set_default_size(400, 300);
+    window.set_default_size(200, 300); // Reduced the default size for vertical layout
 
     let search_entry = Entry::new();
     let search_button = Button::with_label("Search");
 
-    let grid = Grid::new();
-    grid.set_column_homogeneous(true);
-    grid.set_row_spacing(10);
-    grid.set_column_spacing(10);
+    // Create a ScrolledWindow to contain the vertical box
+    let scrolled_window = ScrolledWindow::new(None::<&Adjustment>, None::<&Adjustment>);
+    scrolled_window.set_policy(gtk::PolicyType::Automatic, gtk::PolicyType::Automatic);
 
-    let mut row = 0;
-    let mut column = 0;
+    // Create a vertical box to hold the buttons
+    let vertical_box = Box::new(gtk::Orientation::Vertical, 0);
 
     // Load the apps from the JSON file
     let mut apps_json = String::new();
@@ -39,8 +39,8 @@ fn main() {
         .and_then(|mut file| file.read_to_string(&mut apps_json))
         .expect("Failed to read apps.json");
 
-// Parse the JSON into a vector of App
-let apps: Arc<Vec<App>> = Arc::new(serde_json::from_str(&apps_json).expect("Failed to parse apps.json"));
+    // Parse the JSON into a vector of App
+    let apps: Arc<Vec<App>> = Arc::new(serde_json::from_str(&apps_json).expect("Failed to parse apps.json"));
 
     for app in apps.iter().cloned() {
         let button = Button::with_label(&app.name);
@@ -52,26 +52,24 @@ let apps: Arc<Vec<App>> = Arc::new(serde_json::from_str(&apps_json).expect("Fail
                 .expect("Failed to launch app");
         });
 
-        grid.attach(&button, column, row, 1, 1);
-
-        column += 1;
-        if column == 5 {
-            column = 0;
-            row += 1;
-        }
+        // Pack the button into the vertical box
+        vertical_box.pack_start(&button, false, false, 0);
     }
 
-    let main_box = gtk::Box::new(gtk::Orientation::Vertical, 0);
+    // Add the vertical box to the scrolled window
+    scrolled_window.add(&vertical_box);
+
+    let main_box = Box::new(gtk::Orientation::Vertical, 0);
     main_box.pack_start(&search_entry, false, false, 0);
     main_box.pack_start(&search_button, false, false, 0);
-    main_box.pack_start(&grid, true, true, 0);
+    main_box.pack_start(&scrolled_window, true, true, 0);
 
     window.add(&main_box);
 
     // Add search functionality
     search_entry.connect_changed(move |entry| {
         let search_text = entry.get_text().to_string();
-        search_apps(&search_text, &grid, &apps);
+        search_apps(&search_text, &vertical_box, &apps); // Pass the vertical_box for search
     });
 
     window.connect_delete_event(|_, _| {
@@ -84,18 +82,16 @@ let apps: Arc<Vec<App>> = Arc::new(serde_json::from_str(&apps_json).expect("Fail
     gtk::main();
 }
 
-fn search_apps(search_text: &str, grid: &Grid, apps: &Arc<Vec<App>>) {
-    // Remove all existing buttons from the grid
-    grid.foreach(|child| {
-        grid.remove(child);
+fn search_apps(search_text: &str, vertical_box: &Box, apps: &Arc<Vec<App>>) {
+    // Remove all existing buttons from the vertical box
+    vertical_box.foreach(|child| {
+       vertical_box.remove(child); 
     });
-
-    let mut row = 0;
-    let mut column = 0;
 
     for app in apps.iter().cloned() {
         if app.name.to_lowercase().contains(&search_text.to_lowercase()) {
             let button = Button::with_label(&app.name);
+            let _apps_clone = Arc::clone(apps);
             button.connect_clicked(move |_| {
                 let path = format!("{}{}", SCRIPTS_DIR, app.path);
                 let _ = std::process::Command::new(&app.command)
@@ -104,15 +100,10 @@ fn search_apps(search_text: &str, grid: &Grid, apps: &Arc<Vec<App>>) {
                     .expect("Failed to launch app");
             });
 
-            grid.attach(&button, column, row, 1, 1);
-
-            column += 1;
-            if column == 5 {
-                column = 0;
-                row += 1;
-            }
+            // Pack the button into the vertical box
+            vertical_box.pack_start(&button, false, false, 0);
         }
     }
 
-    grid.show_all();
+    vertical_box.show_all();
 }
